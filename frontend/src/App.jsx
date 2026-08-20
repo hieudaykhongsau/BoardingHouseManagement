74f1efd65dc7a9bbe8e3e519ed1b451c67ff7135
Nguyễn Minh Hiếu <hieumastermind@gmail.com>
2026-08-20 22:53:48 +0700

    remove component Login in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
 import Invoices from './pages/Invoices';
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/login" element={<Login />} />
       <Route path="/invoices" element={<Invoices />} />
     </Routes>
   );
